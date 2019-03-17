8d71a66e2e20ce56dd0eafd327bc1bba248556c8
Dima Murugov <dimamurugov@gmail.com>
2019-03-17 16:50:43 +0300

    Created an even number function

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,13 +1,13 @@
 function Mass(arr){
 	var arr1 = [0,0,0];
 	for(var i = 0; i < arr.length; i++){
-		if ( arr[i] == 0 ) {arr1[2] = arr1[2] + 1}
-		else {
-			if ( arr[i]/2 == 0 ) {arr1[0] = arr1[0] + 1}
-			else {arr1[2] = arr1[1] + 1};
-		};
+		if (typeof arr[i] != "number") continue;
+		if (arr[i] == 0) {
+			arr1[2] = arr1[2] + 1;
+		} else if (arr[i]%2 == 0) {
+			arr1[0] = arr1[0] + 1;
+			} else {arr1[1] = arr1[1] + 1};
 	};
-	
-	console.log(arr1);	
+	return arr1;	
 };
 module.exports = Mass;
