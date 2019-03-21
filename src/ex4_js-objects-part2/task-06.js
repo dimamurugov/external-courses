ba3f5da0ca7a8c8c54f1d3a2a56a9b05c9d7d09e
Dima Murugov <dimamurugov@gmail.com>
2019-03-21 17:52:50 +0300

    Created function zoom all first letters

---
+++ src/ex4_js-objects-part2/task-06.js
@@ -0,0 +1,17 @@
+function zoomFirstLetters(str) {
+	var str1 = "";
+	if (typeof str[0] === 'string') {
+		str1 = str1 + str[0].toUpperCase();
+	};
+	for (var i = 1; i < str.length; i++) {
+
+		if ((str[i-1] == " ") && (typeof str[i] == 'string')) {
+			str1 = str1 + str[i].toUpperCase();
+		} else {
+			str1 = str1 + str[i];
+		};
+  }
+	return str1
+}
+
+module.exports = zoomFirstLetters;
