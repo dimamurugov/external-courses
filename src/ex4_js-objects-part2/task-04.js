92a9b71f3b28a8324d6c44a2d02511b36ef30b45
Dima Murugov <dimamurugov@gmail.com>
2019-03-21 15:44:08 +0300

    Creted 3 function:1)Remove space 2)change first letter to upper case 3)search string in another string

---
+++ src/ex4_js-objects-part2/task-04.js
@@ -0,0 +1,6 @@
+function upFirstRegister(str) { 
+	var temp;
+	temp = str[0].toUpperCase() + str.slice(1);
+	return temp;
+} 
+module.exports = upFirstRegister;
---
+++ src/ex4_js-objects-part2/task-05.js
@@ -0,0 +1,17 @@
+function searchStrInStr(str, str1) {
+	flag = true;
+	j = 0;
+	for(var i = 0; i < str.length-1; i++) {
+		if (str[i] === str1[j]) {
+			flag = true;
+			j++;
+			if (j > str1.length-1) return flag;
+		} else { 
+			flag = false;
+			j = 0;
+		}
+	}
+	return flag;
+} 
+
+module.exports = upFirstRegister
