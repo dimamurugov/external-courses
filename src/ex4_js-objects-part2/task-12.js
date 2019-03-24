d2998ddee64f3e6b24a0e2339d6b1fb21925031a
Dima Murugov <dimamurugov@gmail.com>
2019-03-24 14:57:35 +0300

    Created 3 function: 1)Sum two numbers 2)Random number from 0 to 100 3) Random number from min to max

---
+++ src/ex4_js-objects-part2/task-12.js
@@ -0,0 +1,4 @@
+function toRoundingAmount(a,b) {
+		return +(a+b).toFixed(3);
+}
+module.exports = toRoundingAmount;
---
+++ src/ex4_js-objects-part2/task-13.js
@@ -0,0 +1,4 @@
+function toRandom() {
+	return Math.random()*100;
+}
+module.exports = toRandom;
