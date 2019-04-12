f39703f209abebdacc53042e2304f43fd01ea170
Dima Murugov <dimamurugov@gmail.com>
2019-04-12 12:39:55 +0300

    Did local variable

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,28 +1,29 @@
-var result = 0;
-var Calculator = function() {};
+function Calculator() {};
+
+Calculator.result = 0;
 
 Calculator.getResult = function() {
-	return result
+	return Calculator.result
 };
 Calculator.add = function(value) {
-	if (typeof value !== 'undefined') {result += value;}
+	if (typeof value !== 'undefined') {Calculator.result += value;}
 			return Calculator.add
 };
 Calculator.subtract = function(value) {
-	if (typeof value !== 'undefined') {result -= value;}
+	if (typeof value !== 'undefined') {Calculator.result -= value;}
 			return Calculator.subtract
 };
 Calculator.divide= function(value) {
-	if (typeof value !== 'undefined') {result /= value;}
+	if (typeof value !== 'undefined') {Calculator.result /= value;}
 			return Calculator.divide
 };
 Calculator.multiply= function(value) {
-	if (typeof value !== 'undefined') {result *= value;}
+	if (typeof value !== 'undefined') {Calculator.result *= value;}
 			return Calculator.multiply
 };
 Calculator.reset = function() {
-			result = 0;
-			return 
+			Calculator.result = 0;
 };
+
 
 module.exports = Calculator;
