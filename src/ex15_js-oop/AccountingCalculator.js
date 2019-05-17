d18271b73267f9ef82cfd968013654d038aace78
Dima Murugov <dimamurugov@gmail.com>
2019-05-17 19:38:25 +0300

    Inheritance Calculator

---
+++ src/ex15_js-oop/AccountingCalculator.js
@@ -0,0 +1,19 @@
+function AccountingCalculator(name) {
+     Calculator.apply(this, arguments);
+}
+
+AccountingCalculator.prototype = Object.create(Calculator.prototype);
+AccountingCalculator.prototype.constructor = AccountingCalculator;
+
+AccountingCalculator.prototype.round = function(value) {
+    if (this.__isNumeric(value)) {
+        this.__result = +value.toFixed(3);
+    }
+
+     return this;
+};
+var accountCal = new AccountingCalculator("Бухгалтерский калькулятор");
+
+accountCal.round(1.1234567);
+console.log("Бухгалтерский калькултор:");
+console.log(accountCal.getResult());
---
+++ src/ex15_js-oop/simpleCalculator.js
@@ -0,0 +1,51 @@
+function SimpleCalculator(name) {
+  Calculator.apply(this, arguments);
+}
+
+SimpleCalculator.prototype = Object.create(Calculator.prototype);
+SimpleCalculator.prototype.constructor = SimpleCalculator;
+
+SimpleCalculator.prototype.add = function(num) {
+        if (this.__isNumeric(num)) {
+            this.__result += num;
+        }
+        return this;
+    };
+
+SimpleCalculator.prototype.subtract = function(num) {
+        if (this.__isNumeric(num)) {
+            this.__result -= num;
+        }
+        return this;
+    };
+
+SimpleCalculator.prototype.multiply = function(num) {
+        if (this.__isNumeric(num)) {
+            this.__result *= num;
+        }
+        return this;
+    };
+
+SimpleCalculator.prototype.divide = function(num) {
+        if (this.__isNumeric(num)) {
+            this.__result /= num;
+        }
+        return this;
+    };
+var calculatorSimple = new SimpleCalculator("Обычный калькулятор");
+var resultCalculater = calculatorSimple.add(100)
+    .multiply(2)
+    .divide(20)
+    .reset()
+    .subtract(1)
+    .getResult();
+
+
+    console.log("Обычный калькултор:");
+    console.log(resultCalculater);
+
+    calculatorSimple.setState(1);
+    console.log(calculatorSimple.getResult());
+
+    calculatorSimple.reset();
+    console.log(calculatorSimple.getResult());
