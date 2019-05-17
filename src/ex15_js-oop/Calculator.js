8165214f56474b842fcedbadec92c6ee616a7db1
Dima Murugov <dimamurugov@gmail.com>
2019-05-17 19:10:35 +0300

    Inheritance Calculator

---
+++ src/ex15_js-oop/Calculator.js
@@ -0,0 +1,25 @@
+function Calculator(name) {
+  this.__name = name;
+    this.__result = 0;
+
+}
+ Calculator.prototype.__isNumeric = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+ Calculator.prototype.reset = function() {
+    this.__result = 0;
+    return this;
+};
+
+ Calculator.prototype.getResult = function() {
+    return this.__result;
+};
+
+ Calculator.prototype.setState = function(value) {
+    if(this.__isNumeric(value)) {
+        this.__result = value;
+    }
+
+     return this;
+};
